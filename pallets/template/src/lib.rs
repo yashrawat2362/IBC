fb2f47ef0530387735080d4f14d17d40a42e9e47
aideepakchaudhary <aideepakchaudhary@gmail.com>
2023-05-15 13:46:42 +0530

    added-expire_time

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -29,6 +29,7 @@ pub mod pallet {
 	use super::*;
 	use crate::Error::MemberAlreadyRequested;
 	use frame_support::pallet_prelude::*;
+	use frame_support::traits::OnTimestampSet;
 	use frame_system::pallet_prelude::*;
 
 	#[pallet::pallet]
@@ -56,16 +57,40 @@ pub mod pallet {
 	pub type Proposal<T: Config> =
 		StorageMap<_, Identity, T::Hash, Vote<T::AccountId>, OptionQuery>;
 
+	#[pallet::storage]
+	#[pallet::getter(fn proposaltime)]
+	pub type ProposalTime<T: Config> =
+	StorageMap<_, Identity, T::BlockNumber, T::Hash, OptionQuery>;
+
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		MemberRequestedToJoin { who: T::AccountId },
+		MemberAddedToDao { who: T::AccountId },
 	}
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
 		MemberAlreadyRequested,
+		MemberAlreadyPresentInDao,
+	}
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_initialize(n: BlockNumberFor<T>) -> Weight {
+			let is_proposal_expire = ProposalTime::<T>::contains_key(n);
+
+			if is_proposal_expire {
+				let Propsal_id = ProposalTime::<T>::get(n).unwrap();
+				ProposalTime::<T>::remove(n);
+				Self::deposit_event(Event::ProposalIdRemoved{
+					id: Propsal_id,
+				})
+			}
+
+			Weight::zero()
+		}
 	}
 
 	#[pallet::call]
@@ -93,7 +118,18 @@ pub mod pallet {
 
 		#[pallet::call_index(1)]
 		#[pallet::weight(10_000)]
-		pub fn add_into_dao_user(origin: OriginFor<T>, who: T::AccountId) -> DispatchResult {
+		pub fn add_requested_user_into_dao_member(origin: OriginFor<T>, who: T::AccountId) -> DispatchResult {
+			ensure_root(origin)?;
+
+			let mut all_dao_member = DaoUsers::<T>::get();
+			let index = all_dao_member.binary_search(&who).err().ok_or(Error::<T>::MemberAlreadyPresentInDao)?;
+
+			all_dao_member.insert(index, who.clone());
+
+			DaoUsers::<T>::put(all_dao_member);
+
+			Self::deposit_event(Event::<T>::MemberAddedToDao {who});
+
 			Ok(())
 		}
 
@@ -118,5 +154,22 @@ pub mod pallet {
 		pub fn check_status_of_proposal(origin: OriginFor<T>, proposal: T::Hash) -> DispatchResult {
 			Ok(())
 		}
+
+		#[pallet::call_index(5)]
+		#[pallet::weight(10_000)]
+		pub fn set_proposal_time(origin: OriginFor<T>, proposal: T::Hash, time_duration_in_days: u32) -> DispatchResult {
+
+			ensure_signed(origin.clone())?;
+
+			let prod_block_per_sec = 6;
+			let block_per_day = 14_400;
+			let total_block = block_per_day * time_duration_in_days;
+
+			let mut expire_block =  frame_system::Pallet::<T>::block_number() + total_block.into();
+
+			ProposalTime::<T>::insert(expire_block, proposal);
+			Ok(())
+		}
+
 	}
 }
